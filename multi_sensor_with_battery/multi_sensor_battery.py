f682273815cb1a618ec1d5c03c46b648d7cea54d
Namay Jindal <namayyjindal@gmail.com>
2024-07-04 13:11:53 +0530

    Update multi_sensor_battery.py

--- multi_sensor_with_battery/multi_sensor_battery.py
+++ multi_sensor_with_battery/multi_sensor_battery.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from bleak import BleakClient, BleakScanner
 import logging
 import os
+from collections import defaultdict
 
 # Sensor configurations
 SENSOR_CONFIGS = {
@@ -23,37 +24,38 @@ SENSOR_CONFIGS = {
         "prefix": "left_hand"
     },
     3: {
-        "name": "Sense Ball",
-        "service_uuid": "9E400001-C5C3-E393-B0A9-E50E24DCCA9E",
-        "char_uuid": "9E400002-C5C3-E393-B0A9-E50E24DCCA9E",
-        "reference_uuid": "9E400003-C5C3-E393-B0A9-E50E24DCCA9E",
-        "prefix": "ball"
-    },
-    4: {
         "name": "Sense Right Leg",
         "service_uuid": "7E400001-A5B3-C393-D0E9-F50E24DCCA9E",
         "char_uuid": "7E400002-A5B3-C393-D0E9-F50E24DCCA9E",
         "reference_uuid": "7E400003-A5B3-C393-D0E9-F50E24DCCA9E",
-        "prefix": "right_leg"
+        "prefix": "right_leg",
     },
-    5: {
+    4: {
         "name": "Sense Left Leg",
         "service_uuid": "6E400001-B5C3-D393-A0F9-E50F24DCCA9E",
         "char_uuid": "6E400002-B5C3-D393-A0F9-E50F24DCCA9E",
         "reference_uuid": "6E400003-B5C3-D393-A0F9-E50F24DCCA9E",
         "prefix": "left_leg"
+    },
+    5: {
+        "name": "Sense Ball",
+        "service_uuid": "9E400001-C5C3-E393-B0A9-E50E24DCCA9E",
+        "char_uuid": "9E400002-C5C3-E393-B0A9-E50E24DCCA9E",
+        "reference_uuid": "9E400003-C5C3-E393-B0A9-E50E24DCCA9E",
+        "prefix": "ball"
+    }
 }
-}
+
 
 # Exercise configurations
 EXERCISE_CONFIGS = {
-    "single_hand": [1],  # Only right hand sensor
-    "both_hands": [1, 2],  # Right and left hand sensors
-    "hands_and_ball": [1, 2, 3],  # All sensors
+    "single_hand": [5],  # Only right hand sensor
+    "both_hands": [3, 4],  # Right and left hand sensors
+    "hands_and_ball": [1, 2, 4, 5],  # All sensors
     "all_sensors": [1, 2, 3, 4, 5]  # All sensors
 }
 
-csv_filename = "3_all_sensors_data.csv"
+csv_filename = "farwith4_better_syncing_trial.csv"
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -81,41 +83,49 @@ def create_csv_file(sensor_ids):
 
 class SensorData:
     def __init__(self, sensor_ids):
-        self.data = {i: {} for i in sensor_ids}
+        self.data = defaultdict(list)
         self.sensor_ids = sensor_ids
 
-    def add_data(self, sensor_index, index, values):
-        self.data[sensor_index][index] = values
-        logger.debug(f"Added data for sensor {sensor_index}, index {index}")
+    def add_data(self, sensor_index, timestamp, values):
+        self.data[sensor_index].append((timestamp, values))
+    
+    def get_synced_data(self):
+        synced_data = []
+        min_timestamp = min(min(self.data[sensor_index])[0] for sensor_index in self.sensor_ids)
+        for sensor_index in self.sensor_ids:
+            synced_readings = [entry for entry in self.data[sensor_index] if entry[0] >= min_timestamp]
+            if synced_readings:
+                synced_data.append(synced_readings[0])
+        if len(synced_data) == len(self.sensor_ids):
+            return synced_data
+        return None
 
-    def get_complete_data(self, index):
-        return [self.data[i].get(index) for i in self.sensor_ids]
-
-    def pop_data(self, index):
-        return [self.data[i].pop(index, None) for i in self.sensor_ids]
-
-sensor_data = None
+    def pop_synced_data(self):
+        synced_data = self.get_synced_data()
+        if synced_data:
+            for sensor_index in self.sensor_ids:
+                self.data[sensor_index] = self.data[sensor_index][1:]
+            return synced_data
+        return None
 
 async def notification_handler(sender, data, sensor_index):
     global sensor_data
     try:
         timestamp, index_value, accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, battery_percentage = struct.unpack('<IIffffffB', data)
         timestamp_sec = timestamp / 1000
-        values = [timestamp_sec, index_value] + [round(val, 4) for val in [accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z]] + [battery_percentage]
-        sensor_data.add_data(sensor_index, index_value, values)
+        values = [timestamp_sec, index_value] + [round(value, 4) for value in [accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z]] + [battery_percentage]
+        sensor_data.add_data(sensor_index, timestamp_sec, values)
         
-        complete_data = sensor_data.get_complete_data(index_value)
-        logger.debug(f"Complete data for index {index_value}: {complete_data}")
-        if all(complete_data):
-            combined_data = [item for sublist in complete_data for item in sublist]
+        complete_data = sensor_data.get_synced_data()
+        if complete_data:
+            combined_data = [item for sublist in complete_data for item in sublist[1]]
             with open(csv_filename, 'a', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(combined_data)
-            logger.info(f"Written combined data for index {index_value}")
-            sensor_data.pop_data(index_value)
+            sensor_data.pop_synced_data()
     except Exception as e:
         logger.error(f"Error in notification handler: {e}")
-        
+
 async def write_reference_timestamp(client, reference_uuid):
     reference_timestamp = int(datetime.now().timestamp())
     reference_data = struct.pack('<I', reference_timestamp)
@@ -149,24 +159,24 @@ async def main():
     logger.info(f"Selected exercise: {exercise_choice}, using sensors: {sensor_ids}")
 
     devices = await BleakScanner.discover()
-    sense_balls = []
+    sensors = []
     
     for sensor_id in sensor_ids:
         config = SENSOR_CONFIGS[sensor_id]
         device = next((d for d in devices if d.name == config['name']), None)
         if device:
-            sense_balls.append((device, config))
+            sensors.append((device, config))
         else:
             logger.error(f"Sensor {config['name']} not found.")
     
-    if not sense_balls:
+    if not sensors:
         logger.error("No sensors found for the selected exercise.")
         return
 
     sensor_data = SensorData(sensor_ids)
     create_csv_file(sensor_ids)
 
-    tasks = [connect_and_run(device, config, sensor_id) for sensor_id, (device, config) in zip(sensor_ids, sense_balls)]
+    tasks = [connect_and_run(device, config, sensor_id) for sensor_id, (device, config) in zip(sensor_ids, sensors)]
     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
